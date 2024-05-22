be1a2c8dc4b68ab61cd7e25e8921d97b85e84879
Soubhik Kumar Gon <b422056@iiit-bh.ac.in>
2024-05-22 16:17:06 +0530

    error types

--- @shared/src/errorHandlers.ts
+++ @shared/src/errorHandlers.ts
@@ -1,46 +1,89 @@
-import { StatusCodes } from "http-status-codes"
+import { StatusCodes } from 'http-status-codes';
 
 export interface IErrorResponse {
-    message: string;
-    statusCode: number;
-    status: string;
-    comingFrom: string;
-    serializeErrors(): IError;
+  message: string;
+  statusCode: number;
+  status: string;
+  comingFrom: string;
+  serializeErrors(): IError;
 }
 
-
 export interface IError {
-    message: string;
-    statusCode: number;
-    status: string;
-    comingFrom: string;
+  message: string;
+  statusCode: number;
+  status: string;
+  comingFrom: string;
 }
 
 export abstract class CustomError extends Error {
-    abstract statusCode: number;
-    abstract status: string;
-    comingFrom: string;
+  abstract statusCode: number;
+  abstract status: string;
+  comingFrom: string;
 
-    constructor(message: string, comingFrom: string) {
-        super(message);
-        this.comingFrom = comingFrom;
-    }
+  constructor(message: string, comingFrom: string) {
+    super(message);
+    this.comingFrom = comingFrom;
+  }
 
-    serializeErrors(): IError {
-        return {
-            message: this.message,
-            comingFrom: this.comingFrom,
-            status: this.status,
-            statusCode: this.statusCode,
-        }
+  serializeErrors(): IError {
+    return {
+      message: this.message,
+      statusCode: this.statusCode,
+      status: this.status,
+      comingFrom: this.comingFrom,
     }
+  }
 }
 
 export class BadRequestError extends CustomError {
-    statusCode = StatusCodes.BAD_REQUEST;
-    status = 'error';
+  statusCode = StatusCodes.BAD_REQUEST;
+  status = 'error';
 
-    constructor(message: string, comingFrom: string) {
-        super(message, comingFrom);
-        }
+  constructor(message: string, comingFrom: string) {
+    super(message, comingFrom);
+  }
 }
+
+export class NotFoundError extends CustomError {
+  statusCode = StatusCodes.NOT_FOUND;
+  status = 'error';
+
+  constructor(message: string, comingFrom: string) {
+    super(message, comingFrom);
+  }
+}
+
+export class NotAuthorizedError extends CustomError {
+  statusCode = StatusCodes.UNAUTHORIZED;
+  status = 'error';
+
+  constructor(message: string, comingFrom: string) {
+    super(message, comingFrom);
+  }
+}
+
+export class FileTooLargeError extends CustomError {
+  statusCode = StatusCodes.REQUEST_TOO_LONG;
+  status = 'error';
+
+  constructor(message: string, comingFrom: string) {
+    super(message, comingFrom);
+  }
+}
+
+export class ServerError extends CustomError {
+  statusCode = StatusCodes.SERVICE_UNAVAILABLE;
+  status = 'error';
+
+  constructor(message: string, comingFrom: string) {
+    super(message, comingFrom);
+  }
+}
+
+export interface ErrnoException extends Error {
+  errno?: number;
+  code?: string;
+  path?: string;
+  syscall?: string;
+  stack?: string;
+}   
